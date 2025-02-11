8da469065983ddf173380102cd84e9d4f59bd861
tahliahouston27 <121855279+tahliahouston27@users.noreply.github.com>
2025-02-11 22:11:05 +0200

    Initial commit for Unity game

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore()
+    {
+        playerScore = playerScore + 1;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
---
+++ Assets/birdScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class birdScript : MonoBehaviour
+{
+
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
